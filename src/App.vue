6a40f98c7399a5372510211b994a40a326b3d782
Mohammed Ibrahim <obietrice734@gmail.com>
2020-05-14 23:45:37 +0200

    =api set

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,104 @@
 <template>
-  <div class="" id="app">
-   
+<div>
+  <div class="flex items-center justify-center mt-10 " id="app">
+   <div class="search w-3/5 ml-20">
+            <div class="relative w-full">
+                <div class="absolute inset-y-0 left-0 flex items-center mb-1 ">
+                    <svg class="h-5 w-5" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M8 4C5.79086 4 4 5.79086 4 8C4 10.2091 5.79086 12 8 12C10.2091 12 12 10.2091 12 8C12 5.79086 10.2091 4 8 4ZM2 8C2 4.68629 4.68629 2 8 2C11.3137 2 14 4.68629 14 8C14 9.29583 13.5892 10.4957 12.8907 11.4765L17.7071 16.2929C18.0976 16.6834 18.0976 17.3166 17.7071 17.7071C17.3166 18.0976 16.6834 18.0976 16.2929 17.7071L11.4765 12.8907C10.4957 13.5892 9.29583 14 8 14C4.68629 14 2 11.3137 2 8Z" fill="#a0aec0"/>
+                    </svg>
+                </div>
+                <input class="block bg-white w-full rounded-full text-gray-800 placeholder-gray-600 pl-8 -ml-2 py-2 mr-2 focus:outline-none focus:bg-gray-200 focus:text-gray-900  mr-4" 
+                type="text" 
+                v-model="query"
+                @keypress="fetchWeather"
+                placeholder="Type city name then press enter..">
+            </div>
+    </div>
   </div>
+    <div class="" v-if="typeof weather.main !='undefined'">
+    <div class="">
+      <div class="">{{weather.name}}, {{ weather.sys.country }}</div>
+      <div class="">{{dateBuilder()}}</div>
+    </div>
+    <div class="weather-box">
+      <div class="temp">{{Math.round(weather.main.temp) }}°c</div>
+      <div class="weather-status">{{weather.weather[0].main}}</div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
 
 export default {
   name: 'App',
-}
+  data(){
+    return{
+      api_key:'c1e9728f518bf2dd5717027100381b7a',
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query: "",
+      weather: {},
+      response:''
+    }
+  },
+     methods: {
+    fetchWeather(e) {
+      if (e.key == "Enter") {
+        fetch(
+          `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+        )
+          .then(res => {
+            return res.json();
+          })
+          .then(this.setResults);
+      }
+    },
+    setResults(results) {
+      this.weather = results;
+    },
+    dateBuilder() {
+      let d = new Date();
+      let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ];
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      ];
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+      return `${day} ${date} ${month} ${year}`;
+    }
+  }
+};
 </script>
 
 <style>
   @import "assets/css/tailwind.css";
+  .cover{
+    background-image: url("./assets/cover.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: fit-content;
+    transition: 0.4s;
+  }
 </style>
