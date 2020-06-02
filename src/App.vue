148a188a1e0365432b1d4df535f7979eaa69d919
Mohammed Ibrahim <obietrice734@gmail.com>
2020-06-02 13:17:21 +0200

    =update design

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
-<div>
-    <div class="flex items-center justify-center mt-10 " id="app">
+<div id="app">
+    <div class="flex items-center justify-center mt-10 ">
     <div class="search w-3/5 ml-20">
-              <div class="relative w-full">
-                  <div class="absolute inset-y-0 left-0 flex items-center mb-1 ">
-                      <svg class="h-5 w-5" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                          <path fill-rule="evenodd" clip-rule="evenodd" d="M8 4C5.79086 4 4 5.79086 4 8C4 10.2091 5.79086 12 8 12C10.2091 12 12 10.2091 12 8C12 5.79086 10.2091 4 8 4ZM2 8C2 4.68629 4.68629 2 8 2C11.3137 2 14 4.68629 14 8C14 9.29583 13.5892 10.4957 12.8907 11.4765L17.7071 16.2929C18.0976 16.6834 18.0976 17.3166 17.7071 17.7071C17.3166 18.0976 16.6834 18.0976 16.2929 17.7071L11.4765 12.8907C10.4957 13.5892 9.29583 14 8 14C4.68629 14 2 11.3137 2 8Z" fill="#a0aec0"/>
-                      </svg>
-                  </div>
-                  <input class="block bg-white w-full rounded-full text-gray-800 text-base placeholder-gray-500 pl-8 -ml-2 py-2 mr-2 focus:outline-none focus:bg-gray-200 focus:text-gray-900  mr-4" 
-                  type="text" 
-                  v-model="query"
-                  @keypress="fetchWeather"
-                  placeholder="Type city name then press enter..">
-              </div>
+        <div class="relative w-full">
+            <div class="absolute inset-y-0 left-0 flex items-center mb-1 ">
+                <svg class="h-5 w-5" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M8 4C5.79086 4 4 5.79086 4 8C4 10.2091 5.79086 12 8 12C10.2091 12 12 10.2091 12 8C12 5.79086 10.2091 4 8 4ZM2 8C2 4.68629 4.68629 2 8 2C11.3137 2 14 4.68629 14 8C14 9.29583 13.5892 10.4957 12.8907 11.4765L17.7071 16.2929C18.0976 16.6834 18.0976 17.3166 17.7071 17.7071C17.3166 18.0976 16.6834 18.0976 16.2929 17.7071L11.4765 12.8907C10.4957 13.5892 9.29583 14 8 14C4.68629 14 2 11.3137 2 8Z" fill="#a0aec0"/>
+                </svg>
+            </div>
+            <input class="block bg-white w-full rounded-full text-gray-800 text-base placeholder-gray-500 pl-8 -ml-2 py-2 mr-2 focus:outline-none focus:bg-gray-200 focus:text-gray-900  mr-4" 
+            type="text" 
+            v-model="query"
+            @keypress="fetchWeather"
+            placeholder="Type city name">
+        </div>
       </div>
     </div><!-- search -->
 
@@ -49,7 +49,6 @@ export default {
       url_base: "https://api.openweathermap.org/data/2.5/weather",
       query: "",
       weather: {},
-      response:'',
     }
   },
   methods: {
@@ -87,7 +86,7 @@ export default {
 };
 </script>
 
-<style>
+<style >
   @import "assets/css/tailwind.css";
   .cover{
     background-image: url("./assets/cover2.jpg");
